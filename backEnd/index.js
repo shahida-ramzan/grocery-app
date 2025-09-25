baa611d27d7424a85051ff98db417f9afc96b8cd
Shahida Ramzan <shahidaramzan454@gmail.com>
2025-09-25 09:20:02 +0500

    Connect Database

--- backEnd/index.js
+++ backEnd/index.js
@@ -1,14 +1,23 @@
 import express from 'express';
 import basketRoutes from './src/Routes/route.js'
+import dotenv from 'dotenv';
+import pool from './src/Config/db.js'
+dotenv.config();
 
 const app = express();
 app.use(express.json());
-const PORT = 3000;
+
+
+const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res)=>{
     res.send('Grocery APP');
 });
  app.use("/basket", basketRoutes);
+ app.get("/checkdb",async (req, res)=>{
+    const client = await pool.connect();
+    res.send("check console for db");
+});
 
 app.listen(PORT, ()=> {
 console.log(`My Server is runing on PORT: ${PORT}` );
